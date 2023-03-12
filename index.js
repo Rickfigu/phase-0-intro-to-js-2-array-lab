f4208c550ecd0aabd699c7a7ffe39f9bd03e246d
Rick Figueroa <rfig2k17@gmail.com>
2023-03-12 05:03:52 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+//remove the last element of an array using pop()
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+//remove the first element of an array using shift()
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+function appendCat(name){
+    const Cats = cats.slice();
+    Cats.push(name);
+    return Cats;
+}
+
+function prependCat(name){
+    const Cats = cats.slice();
+    Cats.unshift(name);
+    return Cats;
+}
+
+function removeLastCat(name){
+    const Cats = cats.slice();
+    Cats.pop(name);
+    return Cats;
+}
+
+function removeFirstCat(name){
+    const Cats = cats.slice();
+    Cats.shift(name);
+    return Cats;
+}
